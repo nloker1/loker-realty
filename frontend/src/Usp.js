de1980b832ce4aa5ec6fff4b56622ea9526e7706
Nate Loker <nloker1@gmail.com>
2025-02-28 10:10:08 -0800

    Small Usp tweak

--- frontend/src/Usp.js
+++ frontend/src/Usp.js
@@ -9,12 +9,17 @@ const Usp = () => {
 
         <h2>1. Independent & Fair:</h2>
         <p>
-          Unlike agents tied to big brokerages, I am affiliated with real broker a hands-off agency
-          that allows me more independece. This allows me to give you 
-          <strong> honest, unbiased </strong> advice. My <strong> fair pricing model </strong> 
-          keeps more of your hard-earned equity in your pocket.
+        Unlike agents tied to big brokerages, I am affiliated with Real Broker—a hands-off agency  
+        that allows me greater independence. This enables me to provide you with  
+        <strong>honest, unbiased</strong> advice. My <strong>fair pricing model</strong>  
+        helps keep more of your hard-earned equity in your pocket.  
         </p>
-
+        <h2>2. Professional Marketing:</h2>
+        <p>
+          Every listing receives <em> top-tier marketing</em>, including 
+          <strong> professional photography, videography, and premium placements </strong> on Zillow, 
+          Redfin, and other top real estate platforms—ensuring maximum exposure.
+        </p>
         <h2>3. Local Expertise:</h2>
         <p>
           With deep expertise in <strong> Columbia Gorge land use laws, permits, and builder connections</strong>, 
@@ -22,17 +27,12 @@ const Usp = () => {
           <strong> unlock opportunities </strong> that other agents might miss.
         </p>
 
-        <h2>2. Strong Negotiator:</h2>
+        <h2>4. Strong Negotiator:</h2>
         <p>
           I always negotiate to secure the best possible deal for my clients. Whether you're selling 
           or buying, my skills help you <strong> maximize value </strong> and avoid costly mistakes.
         </p>
-        <h2>4. Professional Marketing:</h2>
-        <p>
-          Every listing receives <em> top-tier marketing</em>, including 
-          <strong> professional photography, videography, and premium placements </strong> on Zillow, 
-          Redfin, and other top real estate platforms—ensuring maximum exposure.
-        </p>
+
         
       </div>
     </div>
